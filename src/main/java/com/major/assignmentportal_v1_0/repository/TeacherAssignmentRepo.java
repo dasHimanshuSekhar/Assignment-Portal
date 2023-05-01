32e632faab076d62f85db0b0f7df4d5e49293446
Himanshu Sekhar Das <61196311+dasHimanshuSekhar@users.noreply.github.com>
2023-05-01 16:10:26 +0530

    Codes
    Codes

---
+++ src/main/java/com/major/assignmentportal_v1_0/repository/TeacherAssignmentRepo.java
@@ -0,0 +1,6 @@
+package com.major.assignmentportal_v1_0.repository;
+
+import com.major.assignmentportal_v1_0.entities.TeacherAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherAssignmentRepo extends JpaRepository<TeacherAssignment, String> {}
